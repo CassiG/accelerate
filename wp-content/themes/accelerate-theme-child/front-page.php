697a998ea57d37697585fc291b4084b4d5d1eda5
CassiGallagher <ccgall@gmail.com>
2015-11-07 11:42:47 -0800

    Add Featured Work Section

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -14,38 +14,60 @@
 
 get_header(); ?>
 
+<!-- Main Homepage Hero -->
 <section class="home-page">
-	
     <div class="site-content">
+		
 		<?php while ( have_posts() ) : the_post(); ?>
 			<div class='homepage-hero'>
 				<?php the_content(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
+        
 	</div><!-- .container -->
-
 </section><!-- .home-page -->
 
-<section class="recent-posts">
-	
+<!-- Featured Work Section -->
+<section class="featured-work">
     <div class="site-content">
-    	<div class="blog-post">
         
-        <h4 class="home-title">From the Blog</h4> <!-- custom blog call -->
-        <?php query_posts('posts_per_page=1'); ?>
-        <?php while ( have_posts() ) : the_post(); ?>
+        	<h4 class="home-title">Featured Work</h4> 
+        	
+			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+        	<?php while ( have_posts() ) : the_post(); ?>
+        	
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <?php the_excerpt(); ?>
+         	
+			<?php endwhile ?> <!-- end blog loop -->
+       		<?php wp_reset_query(); ?> <!-- reset query -->
         
-        	<h2><?php the_title(); ?></h2>
+	</div>
+</section>
+
+
+<!-- From the Blog Section -->
+<section class="recent-posts">
+    <div class="site-content">
+    	
+        <div class="blog-post">
+        
+        	<h4 class="home-title">From the Blog</h4> <!-- custom blog call -->
+        	
+			<?php query_posts('posts_per_page=1'); ?>
+        	<?php while ( have_posts() ) : the_post(); ?>
+        	
+            <h2><?php the_title(); ?></h2>
             <?php the_excerpt(); ?>
             <a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
-        
-        <?php endwhile ?> <!-- end blog loop -->
-        <?php wp_reset_query(); ?> <!-- reset query -->
+       		
+			<?php endwhile ?> <!-- end blog loop -->
+       		<?php wp_reset_query(); ?> <!-- reset query -->
         
         </div>
+        
     </div>
-    
 </section>
 
 <?php get_footer(); ?>
