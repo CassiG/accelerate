29471e4a46c8c794f0884b7c7a5eff156bf0c4aa
CassiGallagher <ccgall@gmail.com>
2015-11-08 15:20:33 -0800

    Create content for Arcive Featured Work page

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -9,13 +9,39 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+<div id="primary" class="site-content">		
+	<div id="content" role="main">
+	
+		<?php while ( have_posts() ) : the_post();
+		
+		 $services=get_field('services');
+		 $client=get_field("client");
+		 $site_link=get_field('link');
+		 $image_1=get_field('image_1');
+		 $size="full";
+		
+		?>
+        
+		<article class="case-study-main">
+                <aside class="case-study-main-sidebar">
+                    <h2><?php the_title(); ?></h2>
+                    <h5><?php echo $services; ?></h5>
+                    <h6><?php echo $client; ?></h6>
+                    
+                    <?php the_excerpt(); ?>
+                    
+                    <p><a href="<?php the_permalink; ?>">View Project</a></p>
+                </aside>
+            
+            <div class="case-study-main-img">
+            	<?php echo wp_get_attachment_image( $image_1, $size ); ?>     
+			</div>
+            
+            </article>
+            <div class="clearfix"></div>
+		<?php endwhile; // end of the loop. ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
